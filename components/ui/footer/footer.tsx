1c76835aafc563f1cbfd59bdf35850cf6607ee73
Marika Basagoitia <marika.basagoitia@gmail.com>
2026-09-08 19:19:02 -0400

    feat(ui): add site footer

---
+++ components/ui/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span>© 2026 Katie Lansdale. All rights reserved.</span>
+    </footer>
+  );
+}
